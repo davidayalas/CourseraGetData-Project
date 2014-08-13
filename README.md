c8b4970d1cb48e904f4bcd4bb15c135f5a81acc3
davidayalas <davixyz@gmail.com>
2014-08-13 15:59:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Getting and Cleaning Data - Course Project  
+===========================================
+
+This is the project assignement from the Coursera course [Getting and Cleaning Data](https://www.coursera.org/course/getdata)
+
+* Dataset analyzed: "Human Activity Recognition Using Smartphones" [(download)](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+* R Script to run: run_analysis.R
+
+Installation
+-------------
+
+1) Unzip and save the directory "UCI HAR Dataset" (or the name of your choose) in your disk
+
+2) Download the run_analysis.R script to your computer. Modify the "srcpath" variable in the first line of the script if the script isn't downloaded into the root of the directory of step 1.
+
+3) Execute the script "Rscript run_analysis.R"
+
+How it works
+-------------
+
+1) The analyzed data is divided into two categories: test data and train data, each one with 3 text files (X data, Y data and Subject data)
+
+2) The script merges this data to get one full dataset to work with and labels the activity value in all rows with the text equivalent to the number origianl code number (from "activity_labels.txt") 
+
+3) With the full dataset, we only need those columns that reference to variables with mean or standard deviation values. To do this, the script reads the "features.txt", select the names with "mean" or "std" string to subset the full dataset of step 2.
+
+4) The script also modifies the new dataset column names to make them more readables.
+
+5) Finally the script makes an aggregation (mean) of the data by "Subject" and "Activity" (then, we get a dataset with only one record by subject and activity) and saves it to a text file named "tidy.txt" in the "scrpath" defined previously. 
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+#datasets directory source
+srcpath = "./"
+
+#Load test files: x, y, subject
+x_test <- read.table(paste0(srcpath,"/test/X_test.txt"))
+y_test <- read.table(paste0(srcpath,"/test/y_test.txt"))
+subject_test <- read.table(paste0(srcpath,"/test/subject_test.txt"))
+
+#bind data frames and change last two columns names to Activity and Subject
+test_data <- cbind(x_test, y_test, subject_test)
+colnames(test_data)[(length(names(test_data))-1)] <- "Activity"
+colnames(test_data)[(length(names(test_data)))] <- "Subject"
+
+#Load test files: x, y, subject
+x_train <- read.table(paste0(srcpath,"/train/X_train.txt"))
+y_train <- read.table(paste0(srcpath,"/train/y_train.txt"))
+subject_train <- read.table(paste0(srcpath,"/train/subject_train.txt"))
+
+#bind data frames and change last two columns names to Activity and Subject
+train_data <- cbind(x_train, y_train, subject_train)
+colnames(train_data)[(length(names(train_data))-1)] <- "Activity"
+colnames(train_data)[(length(names(train_data)))] <- "Subject"
+
+#creates full dataset
+train_test <- rbind(test_data, train_data)
+
+#Labels activity columns with activity description
+activities <- read.table(paste0(srcpath,"/activity_labels.txt"))
+train_test$Activity <- apply(train_test, 1, FUN=function(t) activities[activities$V1==t[(length(t)-1)],2:2])
+
+#subsets columns and labels the variables properly, for mean and standard columns
+features <- read.table(paste0(srcpath,"/features.txt"))
+mean_std <- features[which(grepl("mean", features[[2]], ignore.case=TRUE) | grepl("std", features[[2]], ignore.case=TRUE)),]
+mean_std_total <- train_test[, mean_std[,1:1]]
+
+renameCols <- function(c){ 
+    c=gsub("\\(","",c)
+    c=gsub("\\)","",c)
+    c=gsub("-","",c)
+    c=gsub("tBody","TimeBody",c)
+    c=gsub("fBody","FrequencyBody",c)
+    c=gsub("tGravity","TimeGravity",c)
+    c=gsub("std","Std",c)
+    c=gsub("mean","Mean",c)
+    c=gsub("angle","Angle",c)
+    c=gsub(",gravity","-Gravity",c)
+}
+
+colnames(mean_std_total) <- lapply(mean_std[,2:2],FUN=renameCols)
+mean_std_total$Activity <- train_test$Activity
+mean_std_total$Subject <- train_test$Subject
+
+#data aggregation by Subject and Activity and mean in all variables
+tidy <- aggregate(mean_std_total, by=list(Subject=mean_std_total$Subject, Activity=mean_std_total$Activity), FUN=mean)
+tidy<-tidy[,1:(length(names(tidy))-2)] #removes last two cols (Activity, Subject)
+
+#saves dataset to file
+write.table(tidy, file=paste0(srcpath,"/tidy.txt"),row.names=FALSE)
